fdb99f051f9a333fd2c7945fb430b1d16d18b307
Pratham Chauhan (PC) <prathamchauhan.coding42@gmail.com>
2024-07-12 19:31:29 +0530

    little better, thread write data with lock, output in csv

--- good_proxy_test.py
+++ good_proxy_test.py
@@ -5,6 +5,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+import threading
+import csv
+
 proxysourcebox = [
     'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt',
     'https://raw.githubusercontent.com/MrMarble/proxy-list/main/all.txt',
@@ -12,6 +15,7 @@ proxysourcebox = [
     'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt',
     'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/http/http.txt']
 
+lock = threading.Lock()
 
 proxies = []
 
@@ -25,23 +29,23 @@ headersproxy = {
     'Upgrade-Insecure-Requests': '1',
     'TE': 'Trailers'}
 
-# get free proxies 
+# get free proxies
 for y in proxysourcebox:
     # , verify=False)
     pageprox = requests.get(url=y, timeout=200, headers=headersproxy)
     soupprox = BeautifulSoup(pageprox.content, 'lxml')
-    
+
     print("Proxy Source:", pageprox.url)
     for i in soupprox.text.split('\n'):
         proxies.append(i)
-        
+
 print("Proxy Count:", len(proxies),'\n')
 
 
 def measure_proxy_speed(proxy_url):
     global c
     c += 1
-    print(f"Thread Started: {c}/{proxy_num}", end="\r")
+    print(f"Thread Started: {c}/{proxy_num}, {len(res)}", end="\r")
 
     if not str(proxy_url).startswith('http://'):
         proxy_url = 'http://' + proxy_url
@@ -55,35 +59,49 @@ def measure_proxy_speed(proxy_url):
 
     for _ in range(n):
         response = requests.get('https://example.com/', proxies=proxy, timeout=tolerate_time)
-        if response.status_code != 200: return 
+        if response.status_code != 200: return
 
 
     end_time = time.time()
     diff = end_time - start_time
     diff = diff/n
 
+    # Save the data to a CSV file
+    with lock:
+        with open('fast_proxies.csv', mode='a', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow([proxy_url, diff])
+
 
     print(f"Proxy: {proxy_url:^30}, Response Time: {diff:^20.3f} seconds")
-    res.append((proxy_url, diff))  
-    
+    res.append((proxy_url, diff))
+
+
+''' MAIN '''
+
+# Create the CSV file
+with open('fast_proxies.csv', mode='w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
 
 c = 0 # thread counter
 n = 2 # successive number of requests
-tolerate_time = 15 # in seconds
+tolerate_time = 10 # in seconds
 proxy_num = 1000
 
 res = []    # good proxies list
 random_sub_proxies = random.sample(proxies, proxy_num)
 
-with ThreadPoolExecutor() as executor:
+
+with ThreadPoolExecutor(16) as executor:
     executor.map(measure_proxy_speed, random_sub_proxies)
 
-df = pd.DataFrame(res)
 
+
+df = pd.read_csv('fast_proxies.csv', header=None)
 df = df.sort_values(1)
-fast_proxies = df[0].to_list()
-df.reset_index(drop=True, inplace=True)
 
-df.to_excel('fast_proxies.xlsx', index=False)
+# fast_proxies = df[0].to_list()
+df.reset_index(drop=True, inplace=True)
+df.to_csv('fast_proxies.csv', index=False, header=False)
 
 
